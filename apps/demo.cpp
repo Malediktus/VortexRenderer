e84f3ed73f715baaf5ebf4eb35af05e43806ffe6
Nico Grundei <malediktusrgb@gmail.com>
2023-04-13 13:44:59 +0200

    Removed FPSCamera from demo

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -8,59 +8,6 @@
 #include <cstdlib>
 #include <iostream>
 
-class FPSCamera : public Vortex::Camera {
-public:
-    FPSCamera(float fov, float width, float height) : Camera(fov, width, height) {
-        m_Up = glm::vec3(0.0f, 1.0f, 0.0f);
-        m_Yaw = -90.0f;
-        m_Pitch = 0.0f;
-        OnMouseMoved(0.0f, 0.0f);
-        UpdateMatrices();
-    }
-
-    void OnMouseMoved(float xRel, float yRel) {
-        m_Yaw += xRel * m_MouseSensitivity;
-        m_Pitch -= yRel * m_MouseSensitivity;
-        if (m_Pitch > 89.0f)
-            m_Pitch = 89.0f;
-        if (m_Pitch < -89.0f)
-            m_Pitch = -89.0f;
-
-        glm::vec3 front;
-        front.x = cos(glm::radians(m_Pitch)) * cos(glm::radians(m_Yaw));
-        front.y = sin(glm::radians(m_Pitch));
-        front.z = cos(glm::radians(m_Pitch)) * sin(glm::radians(m_Yaw));
-        m_LookAt = glm::normalize(front);
-        UpdateMatrices();
-    }
-
-    void MoveUp(float amount) {
-        Translate(m_Up * amount);
-    }
-
-    void MoveFront(float amount) {
-        Translate(glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f) * m_LookAt) * amount);
-        UpdateMatrices();
-    }
-
-    void MoveSideways(float amount) {
-        Translate(glm::normalize(glm::cross(m_LookAt, m_Up)) * amount);
-        UpdateMatrices();
-    }
-
-protected:
-    void UpdateMatrices() override {
-        m_View = glm::lookAt(m_Position, m_Position + m_LookAt, m_Up);
-        m_ViewProj = m_Projection * m_View;
-    }
-
-    float m_Yaw;
-    float m_Pitch;
-    glm::vec3 m_LookAt;
-    const float m_MouseSensitivity = 0.1f;
-    glm::vec3 m_Up;
-};
-
 class VortexDemo {
 public:
     VortexDemo() {
@@ -68,8 +15,7 @@ public:
         SetupGlfw();
 
         Vortex::Renderer::Init(m_Window, "../../apps/assets/Shaders/Light.glsl", 1280, 720);
-        m_Camera = std::make_shared<FPSCamera>(90.0f, 1280, 720);
-        m_Camera->MoveFront(-5.0f);
+        m_Camera = std::make_shared<Vortex::Camera>(90.0f, 1280, 720);
         m_MonkeyMesh = std::make_shared<Vortex::Mesh>("../../apps/assets/Objects/Monkey/monkey.obj");
 
         glfwSwapInterval(0);
@@ -81,11 +27,13 @@ public:
         ZoneScoped;
         auto scene = std::make_shared<Vortex::Scene>(m_Camera);
 
-        std::shared_ptr<Vortex::Object> meshObject = std::make_shared<Vortex::Object>();
+        glm::mat4 meshTransform(1.0f);
+        meshTransform = glm::translate(meshTransform, glm::vec3(0.0f, 0.0f, -4.0f));
+        std::shared_ptr<Vortex::Object> meshObject = std::make_shared<Vortex::Object>(meshTransform);
         meshObject->Attach(m_MonkeyMesh);
 
         glm::mat4 lightTransform(1.0f);
-        lightTransform = glm::translate(lightTransform, glm::vec3(1.0f, 0.5f, 1.0f));
+        lightTransform = glm::translate(lightTransform, glm::vec3(1.0f, 0.5f, -3.0f));
 
         std::shared_ptr<Vortex::PointLight> light = std::make_shared<Vortex::PointLight>(1.0f, 1.0f, 1.0f, glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(1.0f));
         std::shared_ptr<Vortex::Object> lightObject = std::make_shared<Vortex::Object>(lightTransform);
@@ -98,7 +46,6 @@ public:
         Vortex::Renderer::Submit(scene);
         Vortex::Renderer::EndFrame();
 
-        ProcessKeyEvents();
         {
             ZoneScopedN("SwapBuffer");
             glfwSwapBuffers(m_Window);
@@ -146,32 +93,6 @@ public:
             std::cout << "Failed to create window!" << std::endl;
             exit(1);
         }
-
-        // glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        glfwSetCursorPosCallback(m_Window, OnMouseMove);
-        glfwGetCursorPos(m_Window, &m_LastMouseX, &m_LastMouseY);
-    }
-
-    void ProcessKeyEvents() {
-        ZoneScoped;
-        if (glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_REPEAT)
-            m_Camera->MoveFront(0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_REPEAT)
-            m_Camera->MoveFront(-0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_REPEAT)
-            m_Camera->MoveSideways(-0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_REPEAT)
-            m_Camera->MoveSideways(0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_SPACE) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_SPACE) == GLFW_REPEAT)
-            m_Camera->MoveUp(0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(m_Window, GLFW_KEY_LEFT_SHIFT) == GLFW_REPEAT)
-            m_Camera->MoveUp(-0.02f);
-        if (glfwGetKey(m_Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-        if (glfwGetKey(m_Window, GLFW_KEY_ENTER) == GLFW_PRESS)
-            glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        if (glfwGetKey(m_Window, GLFW_KEY_Q) == GLFW_PRESS)
-            glfwSetWindowShouldClose(m_Window, true);
     }
 
     ~VortexDemo() {
@@ -185,18 +106,6 @@ private:
         exit(1);
     }
 
-    static void OnMouseMove(GLFWwindow* window, double mouseX, double mouseY) {
-        ZoneScoped;
-        double deltaX = mouseX - m_LastMouseX;
-        double deltaY = mouseY - m_LastMouseY;
-        m_LastMouseX = mouseX;
-        m_LastMouseY = mouseY;
-        if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
-            m_Camera->OnMouseMoved(deltaX, deltaY);
-    }
-
-    static double m_LastMouseX;
-    static double m_LastMouseY;
     GLFWwindow* m_Window;
     uint64_t m_PerfCounterFrequency;
     uint64_t m_LastCounter;
@@ -204,13 +113,9 @@ private:
     float m_Delta = 0.0f;
     uint32_t m_FPS = 0.0f;
 
-    static std::shared_ptr<FPSCamera> m_Camera;
+    std::shared_ptr<Vortex::Camera> m_Camera;
     std::shared_ptr<Vortex::Mesh> m_MonkeyMesh;
 };
-
-double VortexDemo::m_LastMouseX = 0;
-double VortexDemo::m_LastMouseY = 0;
-std::shared_ptr<FPSCamera> VortexDemo::m_Camera;
 
 int main() {
     VortexDemo demo;
